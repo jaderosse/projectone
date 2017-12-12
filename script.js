8a162bcad3eabc52bbe12c0f27464d2e5aedf088
jaderosse <jademrosse@gmail.com>
2017-12-12 13:06:08 -0800

    randomized AI ship.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var opponentBoardContainer = document.getElementById("opponent-board");
 for(var i = 0; i < opRows; i++){
 	for(var j = 0; j < opColumns; j++){
 		var cell = document.createElement("div");
-		opponentBoardContainer.appendChild(cell);
+		opponentBoardContainer.insertBefore(cell, document.getElementById("op-ship"));
 		cell.id = "c" + i + j;
 
 
@@ -23,58 +23,62 @@ for(var i = 0; i < opRows; i++){
 	}
 }
 
+//randomize boat placement by row/column
+
 var playerRows = 8;
 var playerColumns = 8;
 var cellSize = 50;
 var playerHits = 0;
 
 var playerBoardContainer = document.getElementById("player-board");
+var randomShip = document.getElementById("op-ship");
+var rando = document.getElementById("rando");
 
 for(var i = 0; i < playerRows; i++){
 	for(var j = 0; j < playerColumns; j++){
 		var cell = document.createElement("div");
-		playerBoardContainer.appendChild(cell);
-		// cell.id = "c" + i + j;
+		playerBoardContainer.insertBefore(cell, document.getElementById("ship"));
 		cell.className = "cells";
+		cell.id = "s" + i + j;
 		
-
-
-		// set each grid square's coordinates: multiples of the current row or column number
 		var topPosition = j * cellSize;
 		var leftPosition = i * cellSize;			
 		
-		// use CSS absolute positioning to place each grid square on the page
 		cell.style.top = topPosition + 'px';
 		cell.style.left = leftPosition + 'px';	
 	}
 }
 
-// 0 = empty, 1 = part of ship, 2 = hit, 3 = missed (I'll randomize 1s later)
 
-// var opponentBoard = [
-// 	[1, 1, 1, 1, 0, 0, 0, 0],
-// 	[1, 1, 1, 0, 0, 0, 0, 0],
-// 	[1, 1, 1, 0, 0, 0, 0, 0],
-// 	[1, 1, 0, 0, 0, 0, 0, 0],
-// 	[0, 0, 0, 0, 0, 0, 0, 0],
-// 	[0, 0, 0, 0, 0, 0, 0, 0],
-// 	[0, 0, 0, 0, 0, 0, 0, 0],
-// 	[0, 0, 0, 0, 0, 0, 0, 0],
-// ]
+
+
 
 var addClick = opponentBoardContainer.addEventListener("click", cellClicked);
+var aiClick = playerBoardContainer.addEventListener("click", cellClicked);
 
 function cellClicked(e){
 	if(e.target !== e.currentTarget){
 		var row = e.target.id.substring(1, 2);
 		var col = e.target.id.substring(2, 3);
-		console.log("row " + row + " and col " + col + " clicked");
-		//if/else statement for hit or miss
+		// console.log(row, col);
+		if(row === "" && col === ""){
+			console.log("hit ya bitch");
+
+		} else {
+			console.log("missed you");
+			// this.style.backgroundColor = "red";
+		}
+
 	}
 	e.stopPropagation();
 };
 
+var random = rando.addEventListener("click", randomize);
+function randomize(){
+	randomShip.style.transform = "translate" + "("+Math.floor(Math.random()*6)*50 + 'px' + "," + Math.floor(Math.random()*6)*50 + 'px' +")"
 
+	// console.log("translate" + "("+Math.floor(Math.random()*6)*50 + 'px' + "," + Math.floor(Math.random()*6)*50 + 'px' +")");
+}
 
 //DRAGGABILITY - need to figure out how to detect if cell is occupied
 //be able to turn vertical on click
@@ -97,3 +101,9 @@ function handleDrop( event, ui ) {
   ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
   ui.draggable.draggable( 'option', 'revert', false );
 }
+
+//double click ship div to turn vertical
+// var vertical = 
+// $(".draggable").keypress(function(){
+// 	console.log("verticalized!");
+// })
