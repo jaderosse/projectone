5043489764098bfb49515ca959ebcf3eb3779743
jaderosse <jademrosse@gmail.com>
2017-12-11 21:18:23 -0800

    added player board drag/drop.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //creating opponent/player's scoreboard first
-var rows = 8;
-var columns = 8;
+var opRows = 8;
+var opColumns = 8;
 var cellSize = 50;
-var hits = 0;
+var opHits = 0;
 
 var opponentBoardContainer = document.getElementById("opponent-board");
 
-for(var i = 0; i < rows; i++){
-	for(var j = 0; j < columns; j++){
+for(var i = 0; i < opRows; i++){
+	for(var j = 0; j < opColumns; j++){
 		var cell = document.createElement("div");
 		opponentBoardContainer.appendChild(cell);
 		cell.id = "c" + i + j;
@@ -23,18 +23,44 @@ for(var i = 0; i < rows; i++){
 	}
 }
 
+var playerRows = 8;
+var playerColumns = 8;
+var cellSize = 50;
+var playerHits = 0;
+
+var playerBoardContainer = document.getElementById("player-board");
+
+for(var i = 0; i < playerRows; i++){
+	for(var j = 0; j < playerColumns; j++){
+		var cell = document.createElement("div");
+		playerBoardContainer.appendChild(cell);
+		// cell.id = "c" + i + j;
+		cell.className = "cells";
+		
+
+
+		// set each grid square's coordinates: multiples of the current row or column number
+		var topPosition = j * cellSize;
+		var leftPosition = i * cellSize;			
+		
+		// use CSS absolute positioning to place each grid square on the page
+		cell.style.top = topPosition + 'px';
+		cell.style.left = leftPosition + 'px';	
+	}
+}
+
 // 0 = empty, 1 = part of ship, 2 = hit, 3 = missed (I'll randomize 1s later)
 
-var opponentBoard = [
-	[1, 1, 1, 1, 0, 0, 0, 0],
-	[1, 1, 1, 0, 0, 0, 0, 0],
-	[1, 1, 1, 0, 0, 0, 0, 0],
-	[1, 1, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-]
+// var opponentBoard = [
+// 	[1, 1, 1, 1, 0, 0, 0, 0],
+// 	[1, 1, 1, 0, 0, 0, 0, 0],
+// 	[1, 1, 1, 0, 0, 0, 0, 0],
+// 	[1, 1, 0, 0, 0, 0, 0, 0],
+// 	[0, 0, 0, 0, 0, 0, 0, 0],
+// 	[0, 0, 0, 0, 0, 0, 0, 0],
+// 	[0, 0, 0, 0, 0, 0, 0, 0],
+// 	[0, 0, 0, 0, 0, 0, 0, 0],
+// ]
 
 var addClick = opponentBoardContainer.addEventListener("click", cellClicked);
 
@@ -48,3 +74,26 @@ function cellClicked(e){
 	e.stopPropagation();
 };
 
+
+
+//DRAGGABILITY - need to figure out how to detect if cell is occupied
+//be able to turn vertical on click
+
+$(function(){
+  $('.draggable').draggable({
+    containment: 'parent',
+    cursor: 'move',
+    revert: true
+  });
+});
+
+$(function() {
+  $('.cells').droppable({
+    drop: handleDrop
+  });
+});
+
+function handleDrop( event, ui ) {
+  ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+  ui.draggable.draggable( 'option', 'revert', false );
+}
